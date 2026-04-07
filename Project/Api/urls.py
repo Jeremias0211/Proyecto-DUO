cdd05e7099e267db7a102d13214cbb5c20870964
Jeremias Diaz <torresjeremias338@gmail.com>
2026-04-07 00:56:48 -0300

    Proyecto-DUO

---
+++ Project/Api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+# --> Importamos las Vistas
+from .views import *
+
+urlpatterns = [
+    path('', Home, name='home'),
+    path('Productos/', Productos, name='Productos'),
+    path('NuevoProducto/', NuevoProductos, name='nuevo_producto'),
+    path('ModificarProducto/<Codigo>', ModificarProductos, name='Modificar'),
+]
